a860b1901c7e156bc1613eb4cba775a41f6f9f3f
Syaniidikauppias <jouni.winter@gmail.com>
2016-01-19 17:23:14 +0200

    4. tehtävän palautus

--- koodi.rb
+++ koodi.rb
@@ -1,9 +1,4 @@
 # kirjoita koodi tänne
 def tulosta(taulukko)
-  merkkijono = ""
-  for i in 0..taulukko.length
-    merkkijono += taulukko[i].to_s
-    merkkijono += " "
-  end
-  puts merkkijono
+  puts taulukko.join
 end
