3373d9c71e372a9bc77ad61aafda326bce00fd2b
henri <pulperi@gmail.com>
2016-01-23 18:44:43 +0200

    palautus

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,14 @@
 # kirjoita koodi tänne
+def tulosta(array)
+  if array.nil?
+    puts ""
+    elsif array.respond_to?("each")
+    # array is a list of some kind, iterate!
+      puts array.join
+    #    array.each do |i|
+    #      print "#{i}!"
+    #    end
+  else
+    puts "#{array}"
+  end
+end
