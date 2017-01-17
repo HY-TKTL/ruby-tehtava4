e4b71c2bc0a8afee8708c44717a299368dc70aa4
JohannaUha <Uha@Johannas-MacBook-Pro.local>
2017-01-17 17:20:43 +0200

    teht4

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,12 @@
-# kirjoita koodi tänne
+def tulosta(taulukko)
+  lista = ""
+  size = taulukko.length
+  for i in 0..size
+    lista = lista + taulukko.at(i).to_s + " "
+  end
+  puts lista
+end
+
+taulukko = [1, "a", 3]
+
+tulosta(taulukko)
